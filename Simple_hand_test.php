8c12c9616a7677be42b2570360cc15351fe116e6
Michail <futurer@inbox.ru>
2017-06-20 23:35:58 +0400

    Update Simple_hand_test.php

--- Simple_hand_test.php
+++ Simple_hand_test.php
@@ -2,3 +2,26 @@
 /**
 * Простое ручное тестирование
 */
+class UserStore
+{
+    private $users = array();    //массив для хранения пользователей
+
+    public function addUser($name, $mail, $pass)
+    {
+        
+    }
+
+    public function notifyPasswordFailure()
+    {
+    }
+
+    function getUser($mail)
+    {
+    }
+}
+
+//Используем инструментарий:
+$store = new UserStore();
+$store->addUser("Ivan Ivanov", "ivan@mail.ru", "12345");
+$user = $store->getUser("ivan@mail.ru");
+print_r($user);
