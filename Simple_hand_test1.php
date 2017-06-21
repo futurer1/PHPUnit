a162e7f8609bc31a35a1c7be71c1dce41cfa71ae
Michail <futurer@inbox.ru>
2017-06-21 23:57:17 +0400

    Update Simple_hand_test1.php

--- Simple_hand_test1.php
+++ Simple_hand_test1.php
@@ -26,3 +26,29 @@ class Validator
         return false;
     }
 }
+
+/**
+* Проверяемый класс
+*/
+class UserStore
+{
+    private $users = array();    //массив для хранения пользователей
+    public function addUser($name, $mail, $pass)    //добавляет нового пользователя
+    {
+        if(isset($this->users[$mail])){
+            throw new Exception("Пользователь {$mail} уже зарегистрирован");
+        }
+        $this->users[$mail] = array('name' => $name, 'mail' => $mail, 'pass' => $pass);
+        return true;
+    }
+    public function notifyPasswordFailure($mail)    //добавляет к пользователю сообщение об ошибке
+    {
+        if(isset($this->users[$mail])){
+            $this->users[$mail]['failed'] = time();
+        }
+    }
+    function getUser($mail)    //возвращает массив с данными о пользователе
+    {
+        return ($this->users[$mail]);
+    }
+}
