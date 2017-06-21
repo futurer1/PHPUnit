5348a4d6b4f45a7f2f63e54f4f2c12c87886f8f3
Michail <futurer@inbox.ru>
2017-06-21 23:47:59 +0400

    Update Simple_hand_test1.php

--- Simple_hand_test1.php
+++ Simple_hand_test1.php
@@ -1,3 +1,17 @@
+<?php
 /**
 * Использование клиентского класса для тестирования основного класса
 */
+class Validator
+{
+    private $store;
+    
+    public function __construct(UserStore $store)
+    {
+        $this->store = $store;
+    }
+  
+    public function validateUser($mail, $pass)
+    {
+    }
+}
